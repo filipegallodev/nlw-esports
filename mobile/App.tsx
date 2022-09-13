46b7da4f70ec35450618805318142f309be4b8d4
Filipe G <fgallo_dev@hotmail.com>
2022-09-13 13:39:00 -0300

    FIX: Adicionando a pasta mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'react-native';
+
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
+import { Background } from './src/components/Background';
+
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor='transparent'
+        translucent
+      />
+      {fontsLoaded ? <Home /> : <Loading />}
+    </Background>
+  );
+}
---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,36 @@
+import { View, Image, FlatList } from "react-native";
+import { styles } from "./styles";
+
+import { Heading } from "../../components/Heading";
+import { GameCard } from "../../components/GameCard";
+
+import { GAMES } from "../../utils/games";
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <GameCard
+            data={item}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
+      />
+
+    </View>
+  );
+}
